64ace9e9baf67ddbff94e1e001d48ce5f834ca0a
Samet DOGAN <s.doqann1570@gmail.com>
2022-09-14 22:47:37 +0300

    1.Gun Ders Arasi

---
+++ intro/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        //değişken isimleri java'da camelCase yazılır
+        String ortaMetin = "İlginizi çekebilir";
+        String altMetin = "Vade süresi";
+
+        System.out.println(ortaMetin);
+    }
+}
