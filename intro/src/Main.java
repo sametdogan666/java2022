72cdfd1302ab06a9089c058822f442c941d14dd4
Samet DOGAN <s.doqann1570@gmail.com>
2022-09-14 23:16:49 +0300

    If else

--- intro/src/Main.java
+++ intro/src/Main.java
@@ -11,5 +11,23 @@ public class Main {
 
         //integer
         int vade = 12;
+
+        double dolarDun = 18.14;
+        double dolarBugun = 18.20;
+
+        boolean dolarDustuMu = false;
+
+        String okYonu = "";
+
+        if (dolarBugun < dolarDun) { //false
+            okYonu = "down.jpg";
+            System.out.println(okYonu);
+        } else if (dolarBugun > dolarDun) {
+            okYonu = "up.svg";
+            System.out.println(okYonu);
+        } else {
+            okYonu = "equal.svg";
+            System.out.println(okYonu);
+        }
     }
 }
