744a557017ce0d41deebffabfb9cdd8779ba2d90
Samet DOGAN <s.doqann1570@gmail.com>
2022-09-21 23:00:18 +0300

    2.ders 2.ara

---
+++ oop1/src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String mesaj = "Vade oranı";
+
+        Product product1 = new Product();
+        product1.setName("Delongi Kahve Makinesi");
+        product1.setDiscount(7);
+        product1.setUnitPrice(7500);
+        product1.setUnitsInStock(3);
+        product1.setImageUrl("image1.jpg");
+
+        //System.out.println(product1.name);
+
+        Product product2 = new Product();
+        product2.setName("Smeg Kahve Makinesi");
+        product2.setDiscount(7);
+        product2.setUnitPrice(7500);
+        product2.setUnitsInStock(3);
+        product2.setImageUrl("image2.jpg");
+
+
+        Product product3 = new Product();
+        product3.setName("Kitchen Aid Kahve Makinesi");
+        product3.setDiscount(7);
+        product3.setUnitPrice(7500);
+        product3.setUnitsInStock(3);
+        product3.setImageUrl("image3.jpg");
+
+
+        Product[] products = {product1, product2, product3};
+
+        System.out.println("<ul>");
+        for (Product product : products) {
+            System.out.println("<li>" + product.getName() + "</li>");
+        }
+        System.out.println("</ul>");
+    }
+}
