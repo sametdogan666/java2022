a4d48ef7ec81e3bec79747f53429119623ec4af0
Samet DOGAN <s.doqann1570@gmail.com>
2022-09-23 00:57:27 +0300

    30.video sonu

---
+++ day2Homework2/classes/src/CustomerManager.java
@@ -0,0 +1,13 @@
+public class CustomerManager {
+    public void Add(){
+        System.out.println("Müşteri eklendi");
+    }
+
+    public void Remove(){
+        System.out.println("Müşteri silindi");
+    }
+
+    public void Update(){
+        System.out.println("Müşteri güncellendi");
+    }
+}
